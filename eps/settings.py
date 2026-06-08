7312ae62807c557b62f0ceb37cea4c3540f5a8db
Mansi Patel <sjpatel7777@gmail.com>
2026-06-08 17:13:21 +0530

    env changed

--- eps/settings.py
+++ eps/settings.py
@@ -141,8 +141,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='pvrushti71@gmail.com'
-EMAIL_HOST_PASSWORD='mckp zeph cyrc ttpx'
+EMAIL_HOST_USER=os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL=EMAIL_HOST_USER
 
 MEDIA_URL = "/media/"
